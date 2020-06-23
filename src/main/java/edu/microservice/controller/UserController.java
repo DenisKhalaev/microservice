6c639253bd3aa3724899110fdd1b4e44479e7bc9
dkhalaev <dkhalaev@egartech.com>
2020-06-23 17:19:46 +0300

    task2 refactor

--- src/main/java/edu/microservice/controller/UserController.java
+++ src/main/java/edu/microservice/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.*;
+
 @ContextConfiguration({
         "classpath:application.yml"
 })
--- src/main/java/edu/microservice/model/User.java
+++ src/main/java/edu/microservice/model/User.java
@@ -48,10 +48,9 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{ " +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        return "User{ "
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", email='" + email + '\'' + '}';
     }
 }
