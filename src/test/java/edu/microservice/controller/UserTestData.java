ad8e9413caf42efedb8331e77de2fb5afab4dd6a
dkhalaev <dkhalaev@egartech.com>
2020-06-23 16:19:05 +0300

    task2

--- src/test/java/edu/microservice/controller/UserTestData.java
+++ src/test/java/edu/microservice/controller/UserTestData.java
@@ -5,14 +5,14 @@ import edu.microservice.model.User;
 import java.util.Arrays;
 import java.util.List;
 
-public class UserTestData {
+class UserTestData {
 
-    public static final User user1 = new User("Denis", "denis@mail.ru");
-    public static final User user2 = new User("Ann", "Anna@gmail.com");
-    public static final User user3 = new User("Piter", "pit@ya.ru");
-    public static final User user4 = new User("Eliza", "Eliza@mail.ru");
+    private static final User user1 = new User("Denis", "denis@mail.ru");
+    private static final User user2 = new User("Ann", "Anna@gmail.com");
+    private static final User user3 = new User("Piter", "pit@ya.ru");
+    static final User user4 = new User("Eliza", "Eliza@mail.ru");
 
 
-    public static final List<User> userList = Arrays.asList(user1, user2, user3);
+    static final List<User> userList = Arrays.asList(user1, user2, user3);
 
 }
